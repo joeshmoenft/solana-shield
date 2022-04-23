f5c0517d9f7aafad02555035f3b978d8d6474fa6
Jane Doe <x@fjoisjfosjofijsf333330.com>
2022-04-23 16:00:19 -0600

    test redis

--- worker.js
+++ worker.js
@@ -6,6 +6,7 @@ const path = require('path');  require('dotenv').config({ path:path.join(__dirna
 
 const redis = require('redis');
 let REDIS_URL = process.env.REDIS_URL; // || 'redis://127.0.0.1:6379';
+console.log(REDIS_URL);
 const subscriber = redis.createClient({REDIS_URL});
 subscriber.connect();
 
